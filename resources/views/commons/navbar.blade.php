6754b4e71746ffc29ae818fa03598d889d72a124
maho.morita <maho.morita@rakuten.com>
2018-06-22 04:33:22 +0000

    welcome

--- resources/views/commons/navbar.blade.php
+++ resources/views/commons/navbar.blade.php
@@ -8,53 +8,93 @@
                     <span class="icon-bar"></span>
                     <span class="icon-bar"></span>
                 </button>
-                <a class="navbar-left" href="/"><img src="{{ secure_asset("images/logo.png") }}" alt="ikoo"></a>
+                <a class="navbar-left" href="/">ikoo</a>
             </div>
             <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-                <ul class="nav navbar-nav navbar-right">
-                  
+
+<!--navbar-->
+
+<ul class="nav navbar-nav navbar-right">
+  <a href="#" class="dropdown-toggle" data-toggle="dropdown">検索<b class="caret"></b></a>
+  <ul class="dropdown-menu multi-level">
+    
+    <li class="dropdown-submenu">
+    <a href="#" class="dropdown-toggle" data-toggle="dropdown">季節</a>
+            <ul class="dropdown-menu">
+              <li><a href="#">春</a></li>
+              <li><a href="#">夏</a></li>
+              <li><a href="#">秋</a></li>
+              <li><a href="#">冬</a></li>
+            </ul>
+    </li>
+      
+    
+    <li class="dropdown-submenu">
+    <a href="#" class="dropdown-toggle" data-toggle="dropdown">自然</a>
+            <ul class="dropdown-menu">
+              <li><a href="#">山</a></li>
+              <li><a href="#">海</a></li>
+              <li><a href="#">川</a></li>
+              <li><a href="#">森</a></li>
+            </ul>
+    </li>
+    
+    
+  </ul>
+</ul>      
                    
-                        <li class="dropdown">
-                            <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">
-                                <span class="glyphicon glyphicon-signal" aria-hidden="true"></span>
-                                検索
-                                <span class="caret"></span>
-                            </a>
-                            <ul class="dropdown-menu">
-                                <li class="dropdown">季節</li>
-                                    <ul class="dropdown-menu">
-                                        <li><a href="#">春</a></li>
-                                        <li><a href="#">夏</a></li>
-                                        <li><a href="#">秋</a></li>
-                                        <li><a href="#">冬</a></li>
-                                    </ul>
-                                <li><a href="{{ route('ranking.have') }}"></a></li>
-                            </ul>
-                        </li>
+                       
+                        <!--<ul class="nav navbar-nav navbar-right">-->
+                        <!--<li class="dropdown">-->
+                        <!--    <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">-->
+                        <!--        <span class="glyphicon glyphicon-signal" aria-hidden="true"></span>-->
+                        <!--        検索-->
+                        <!--        <span class="caret"></span>-->
+                        <!--    </a>-->
+                            
+                             
+                        <!--    <ul class="dropdown-menu">-->
+                        <!--        <li class="dropdown">-->
+                        <!--            <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">季節</a>-->
+                                
+                        <!--            <ul class="dropdown-menu">-->
+                        <!--                <li><a href="#">春</a></li>-->
+                        <!--                <li><a href="#">夏</a></li>-->
+                        <!--                <li><a href="#">秋</a></li>-->
+                        <!--                <li><a href="#">冬</a></li>-->
+                        <!--            </ul>-->
+                        <!--        </li>-->
+                        <!--        <li class="dropdown">自然</li>-->
+                        <!--            <ul class="dropdown-menu">-->
+                        <!--                <li><a href="#">春</a></li>-->
+                        <!--                <li><a href="#">夏</a></li>-->
+                        <!--                <li><a href="#">秋</a></li>-->
+                        <!--                <li><a href="#">冬</a></li>-->
+                        <!--            </ul>-->
+                        <!--    </ul>-->
+                        <!--</li>-->
                         
-                    @if (Auth::check())
-                        <li class="dropdown">
-                            <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">
-                                <span class="gravatar">
-                                    <img src="{{ Gravatar::src(Auth::user()->email, 20) . '&d=mm' }}" alt="" class="img-circle">
-                                </span>
-                                {{ Auth::user()->name }}
-                                <span class="caret"></span>
-                            </a>
-                            <ul class="dropdown-menu">
-                                <li>
-                                    <a href="{{ route('users.show', Auth::user()->id) }}">マイページ</a>
-                                </li>
-                                <li role="separator" class="divider"></li>
-                                <li>
-                                    <a href="{{ route('logout.get') }}">ログアウト</a>
-                                </li>
-                            </ul>
-                        </li>
-                    @else
-                        <li><a href="{{ route('signup.get') }}">新規登録</a></li>
-                        <li><a href="{{ route('login') }}">ログイン</a></li>
-                    @endif
+                   
+                <!--        <li class="dropdown">-->
+                <!--            <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">-->
+                                
+                               
+                <!--                <span class="caret"></span>-->
+                <!--            </a>-->
+                <!--            <ul class="dropdown-menu">-->
+                <!--                <li>-->
+                <!--                    <a href="#">マイページ</a>-->
+                <!--                </li>-->
+                <!--                <li role="separator" class="divider"></li>-->
+                <!--                <li>-->
+                <!--                    <a href="#">ログアウト</a>-->
+                <!--                </li>-->
+                <!--            </ul>-->
+                <!--        </li>-->
+                   
+                <!--        <li><a href="#">新規登録</a></li>-->
+                <!--        <li><a href="#">ログイン</a></li>-->
+                    
                 </ul>
             </div>
         </div>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,16 +1,16 @@
 @extends('layouts.app')
 
-@section('cover')
+@section('content')
     <div class="cover">
         <div class="cover-inner">
             <div class="cover-contents">
                 <h1>ikoo</h1>
                 <h2>そうだ、神奈川にいこう。</h2>
                 @if (!Auth::check())
-                    <a href="{{ route('signup.get') }}" class="btn btn-success btn-lg">Signup</a>
-                    <a href="{{ route('login.get') }}" class="btn btn-success btn-lg">Login</a>
+                    <a href="#" class="btn btn-success btn-lg">Signup</a>
+                    <a href="#" class="btn btn-success btn-lg">Login</a>
                 @endif
-                    <a href="{{ route('view.get') }}" class="btn btn-success btn-lg">View</a>
+                    <a href="#" class="btn btn-success btn-lg">View</a>
             </div>
         </div>
     </div>
