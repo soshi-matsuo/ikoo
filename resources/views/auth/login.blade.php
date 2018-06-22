40156dd16902bb7e8adeb9cf6a03a413d3157fb3
yuina.fukuoka <yuina.fukuoka@rakuten.com>
2018-06-22 06:48:19 +0000

    facade composer update

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -8,19 +8,19 @@
             <div class="panel-body">
                 {!! Form::open(['route' => 'login.post']) !!}
                     <div class="form-group">
-                        {!! form::label('email', 'メールアドレス') !!}
-                        {!! form::email('email', old('email'), ['class' => 'form-control']) !!}
+                        {!! Form::label('email', 'メールアドレス') !!}
+                        {!! Form::email('email', old('email'), ['class' => 'form-control']) !!}
                     </div>
 
                     <div class="form-group">
-                        {!! form::label('password', 'パスワード') !!}
-                        {!! form::password('password', ['class' => 'form-control']) !!}
+                        {!! Form::label('password', 'パスワード') !!}
+                        {!! Form::password('password', ['class' => 'form-control']) !!}
                     </div>
 
                     <div class="text-right">
-                        {!! form::submit('ログイン', ['class' => 'btn btn-success']) !!}
+                        {!! Form::submit('ログイン', ['class' => 'btn btn-success']) !!}
                     </div>
-                {!! form::close() !!}
+                {!! Form::close() !!}
             </div>
         </div>
     </div>
