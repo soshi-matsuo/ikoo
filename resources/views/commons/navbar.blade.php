79c640a0125d1d96ed5be82734f18b3a58aec776
maho.morita <maho.morita@rakuten.com>
2018-06-22 03:11:00 +0000

    welcome

---
+++ resources/views/commons/navbar.blade.php
@@ -0,0 +1,62 @@
+<header>
+    <nav class="navbar navbar-default navbar-static-top">
+        <div class="container">
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-left" href="/"><img src="{{ secure_asset("images/logo.png") }}" alt="ikoo"></a>
+            </div>
+            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                <ul class="nav navbar-nav navbar-right">
+                  
+                   
+                        <li class="dropdown">
+                            <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">
+                                <span class="glyphicon glyphicon-signal" aria-hidden="true"></span>
+                                検索
+                                <span class="caret"></span>
+                            </a>
+                            <ul class="dropdown-menu">
+                                <li class="dropdown">季節</li>
+                                    <ul class="dropdown-menu">
+                                        <li><a href="#">春</a></li>
+                                        <li><a href="#">夏</a></li>
+                                        <li><a href="#">秋</a></li>
+                                        <li><a href="#">冬</a></li>
+                                    </ul>
+                                <li><a href="{{ route('ranking.have') }}"></a></li>
+                            </ul>
+                        </li>
+                        
+                    @if (Auth::check())
+                        <li class="dropdown">
+                            <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">
+                                <span class="gravatar">
+                                    <img src="{{ Gravatar::src(Auth::user()->email, 20) . '&d=mm' }}" alt="" class="img-circle">
+                                </span>
+                                {{ Auth::user()->name }}
+                                <span class="caret"></span>
+                            </a>
+                            <ul class="dropdown-menu">
+                                <li>
+                                    <a href="{{ route('users.show', Auth::user()->id) }}">マイページ</a>
+                                </li>
+                                <li role="separator" class="divider"></li>
+                                <li>
+                                    <a href="{{ route('logout.get') }}">ログアウト</a>
+                                </li>
+                            </ul>
+                        </li>
+                    @else
+                        <li><a href="{{ route('signup.get') }}">新規登録</a></li>
+                        <li><a href="{{ route('login') }}">ログイン</a></li>
+                    @endif
+                </ul>
+            </div>
+        </div>
+    </nav>
+</header>
